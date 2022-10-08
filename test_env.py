5eebda362269a0f8c52f3faea23dcc131b145c2d
Zachary Hamida <zac.hamida@gmail.com>
2022-10-08 09:07:20 -0400

    added comments

--- test_env.py
+++ test_env.py
@@ -1,6 +1,7 @@
 #%%
 from infra_planner import infra_planner
 
+# fixed_seed (0: random episodes, 1: fixed episodes )
 # infra_lvl (0: element level, 1: category level, 2: bridge level )
 env = infra_planner(fixed_seed=1, infra_lvl=2)
 
